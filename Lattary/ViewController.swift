5dc63047d4cf0b2e0683ca39f80c0d599dfb3203
Mahyar Shakouri <shakourimahyar@outlook.com>
2024-02-20 11:19:56 +0330

    Initial Commit

---
+++ Lattary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lattary
+//
+//  Created by Mahyar on 2/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
